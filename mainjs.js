060d7ec5a1ed8efc9964d97e59f65ef4bf2328a2
IBonaventura <ignaciobonat@gmail.com>
2023-09-11 12:38:59 -0300

    Se agrega el proyecto

---
+++ mainjs.js
@@ -0,0 +1,30 @@
+//objeto para representar el juego 
+const tetrisGame = {
+    canvas: document.getElementById('tetrisCanvas'),
+    ctx: null,
+    
+};
+
+// Función para inicializar el juego
+function initializeGame() {
+    tetrisGame.ctx = tetrisGame.canvas.getContext('2d');
+    
+}
+
+// Función principal para actualizar el juego
+function updateGame() {
+    
+}
+
+
+function gameLoop() {
+    updateGame();
+    drawGame();
+    requestAnimationFrame(gameLoop);
+}
+
+// Llamamos a la función de inicialización cuando se carga la página
+window.addEventListener('load', () => {
+    initializeGame();
+    gameLoop();
+});
