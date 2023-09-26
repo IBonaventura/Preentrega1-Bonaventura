dbb6ee8e578d677a2e950477aab46a483714fe17
IBonaventura <ignaciobonat@gmail.com>
2023-09-26 18:49:47 -0300

    nuevos cambios 3

--- mainjs.js
+++ mainjs.js
@@ -1,50 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Juego Piedra, Papel, Tijeras</title>
-</head>
-<body>
-    <h1>Juego Piedra, Papel, Tijeras</h1>
-    <div>
-        <button id="piedra" style="margin: 5px;">Piedra</button>
-        <button id="papel" style="margin: 5px;">Papel</button>
-        <button id="tijeras" style="margin: 5px;">Tijeras</button>
-    </div>
+const opciones = ['piedra', 'papel', 'tijeras'];
+
+function jugar() {
+    const eleccionUsuario = prompt("Elige una opción: piedra, papel o tijeras").toLowerCase();
     
-    <h2>Resultado:</h2>
-    <p id="resultado"></p>
-    
-    <script>
-        const opciones = ['piedra', 'papel', 'tijeras'];
-        const botones = document.querySelectorAll('button');
-        const resultadoElement = document.getElementById('resultado');
+    if (!opciones.includes(eleccionUsuario)) {
+        alert("Eleccion inválida. Por favor, elige piedra, papel o tijeras.");
+        return;
+    }
 
-        botones.forEach((boton) => {
-            boton.addEventListener('click', () => {
-                const eleccionUsuario = boton.id;
-                const eleccionMaquina = opciones[Math.floor(Math.random() * 3)];
-                const resultado = determinarResultado(eleccionUsuario, eleccionMaquina);
-                mostrarResultado(resultado);
-            });
-        });
+    const eleccionMaquina = opciones[Math.floor(Math.random() * 3)];
+    const resultado = determinarResultado(eleccionUsuario, eleccionMaquina);
+    alert(resultado);
+}
 
-        function determinarResultado(eleccionUsuario, eleccionMaquina) {
-            if (eleccionUsuario === eleccionMaquina) {
-                return "¡Es un empate!";
-            } else if (
-                (eleccionUsuario === 'piedra' && eleccionMaquina === 'tijeras') ||
-                (eleccionUsuario === 'papel' && eleccionMaquina === 'piedra') ||
-                (eleccionUsuario === 'tijeras' && eleccionMaquina === 'papel')
-            ) {
-                return "¡Ganaste!";
-            } else {
-                return `¡Perdiste! La máquina eligió ${eleccionMaquina}`;
-            }
-        }
-
-        function mostrarResultado(resultado) {
-            resultadoElement.textContent = resultado;
-        }
-    </script>
-</body>
-</html>
+function determinarResultado(eleccionUsuario, eleccionMaquina) {
+    if (eleccionUsuario === eleccionMaquina) {
+        return "¡Es un empate!";
+    } else if (
+        (eleccionUsuario === 'piedra' && eleccionMaquina === 'tijeras') ||
+        (eleccionUsuario === 'papel' && eleccionMaquina === 'piedra') ||
+        (eleccionUsuario === 'tijeras' && eleccionMaquina === 'papel')
+    ) {
+        return "¡Ganaste!";
+    } else {
+        return `¡Perdiste! La máquina eligió ${eleccionMaquina}`;
+    }
+}
